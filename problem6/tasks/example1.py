7559aa2c571b9d4337ef7c30e9b87f3ee6317ebd
alex <aok@buran.rest>
2022-04-17 15:34:11 +0300

    task6 ready

---
+++ problem6/tasks/example1.py
@@ -0,0 +1,7 @@
+s = int(input())
+n = 1
+while s < 51:
+    s = s + 5
+    n = n * 2
+
+print(n)
---
+++ problem6/tasks/example2.py
@@ -0,0 +1,8 @@
+d = int(input())
+n = 0
+s = 0
+while s <= 365:
+    s = s + d
+    n = n + 5
+
+print(n)
